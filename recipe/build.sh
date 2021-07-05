9f614168cdcd816c4bf8ef27e9b1075bf8842faa
Matthias Diener <mdiener@illinois.edu>
2021-07-05 18:03:00 -0500

    add pet

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -x
 
 
-./configure --prefix=$PREFIX --enable-shared-barvinok --disable-dependency-tracking
+./configure --prefix=$PREFIX --enable-shared-barvinok --disable-dependency-tracking --with-pet=bundled
 
 make -j${CPU_COUNT}
 
