335d51bf028f954954b61273e3a9a997c9c5f1a9
Matthias Diener <mdiener@illinois.edu>
2021-07-05 15:50:41 -0500

    use system isl

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,10 @@
 set -x
 
 
-./configure --prefix=$PREFIX --enable-shared-barvinok
+./configure --prefix=$PREFIX --enable-shared-barvinok --with-isl=system
 
 make -j${CPU_COUNT}
+
+make check
 
 make install
