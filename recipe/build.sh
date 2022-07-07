f44d921d2cbf0ccd59cdc5b48913712744a6e53a
Matthias Diener <mdiener@illinois.edu>
2022-07-07 09:39:50 -0500

    libcpp

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 cp $BUILD_PREFIX/share/gnuconfig/config.* polylib/
 cp $BUILD_PREFIX/share/gnuconfig/config.* isl/
 
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
 ./configure --prefix=$PREFIX --enable-shared-barvinok --disable-dependency-tracking
 
 make -j${CPU_COUNT}
