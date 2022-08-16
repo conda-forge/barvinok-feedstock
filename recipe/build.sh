7f23b92ca07985ffd6373fdcc8c2f761716ca5e9
Isuru Fernando <idf2@illinois.edu>
2022-08-16 12:32:45 -0500

    Disable generating code for the build machine microarchitecture

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,9 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* isl/
 
 CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
+# Disable generating code for the build machine microarchitecture
+export ax_cv_gcc_x86_cpuid_1=unknown
+
 ./configure --prefix=$PREFIX --enable-shared-barvinok --disable-dependency-tracking
 
 make -j${CPU_COUNT}
